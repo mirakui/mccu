fd365ccef20cf8083ec0c58c47ae0dcaf2296e7d
Issei Naruta <naruta@cookpad.com>
2016-02-09 11:15:17 +0900

    Implement each_matched_key

--- lib/mccu/client.rb
+++ lib/mccu/client.rb
@@ -20,23 +20,27 @@ module Mccu
 
     def purge_matched(pattern)
       matched_count = 0
-      @conn[:text].each_all_key do |key|
-        if pattern === key
-          matched_count += 1
-          @conn[:binary].delete key
-        end
+      each_matched_key(pattern) do |key|
+        matched_count += 1
+        @conn[:binary].delete key
       end
       matched_count
     end
 
     def list_matched(pattern)
       matched_keys = []
-      @conn[:text].each_all_key do |key|
-        if pattern === key
-          matched_keys << key
-        end
+      each_matched_key(pattern) do |key|
+        matched_keys << key
       end
       matched_keys
+    end
+
+    def each_matched_key(pattern, &block)
+      @conn[:text].each_all_key do |key|
+        if pattern === key
+          yield key
+        end
+      end
     end
   end
 end
